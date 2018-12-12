485fa5e91135ebf0f8c4dc52aec4153c2d8dd73b
Firentus Agustone Pasaribu <45529731+firentus999@users.noreply.github.com>
2018-12-12 09:19:35 +0700

    Program

---
+++ Program Penjumlahan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    float hasil;
+    cout<<"masukan bilangan 1 :";
+    cin>>x;
+    cout<<"masukan bilangan 2 :";
+    cin>>y;
+    hasil=x+y;
+    cout<<"hasil Pertambahan adalah "<<hasil;
+    return 0;
+}
